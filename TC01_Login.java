6145b21d2708b0f4549cf006b2ae597b96f04b2a
sriramulu <sriramulumula370@gmail.com>
2022-01-21 13:27:10 +0530

    first commit

---
+++ TC01_Login.java
@@ -0,0 +1,22 @@
+package Day01;
+
+import org.testng.annotations.Test;
+
+public class TC01_Login {
+	
+	int x=10,y=20,z; //Declaration
+	
+  @Test
+  public void LoginTest() {
+	  
+	  z=x+y;
+	  System.out.println("Value of z is :"+z);
+	  
+  }
+  
+  
+  
+  
+  
+  
+}
---
+++ TC02_Login.java
@@ -0,0 +1,9 @@
+package Day01;
+
+import org.testng.annotations.Test;
+
+public class TC02_Login {
+  @Test
+  public void f() {
+  }
+}
